58983fdec4385daceca6817d1257f23dade0fd60
test56tester28tt <test56tester28tt@gmail.com>
2016-12-12 15:05:32 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -332,14 +332,14 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		var company Account
 		companyBytes, err1 := stub.GetState(accountPrefix + args[0])
 		if err1 != nil {
-			fmt.Println("Account not found " + companyID)
-			return company, errors.New("Account not found " + companyID)
+			fmt.Println("Account not found " + args[0])
+			return company, errors.New("Account not found " + args[0])
 		}
 
 		err = json.Unmarshal(companyBytes, &company)
 		if err != nil {
-			fmt.Println("Error unmarshalling account " + companyID + "\n err:" + err.Error())
-			return company, errors.New("Error unmarshalling account " + companyID)
+			fmt.Println("Error unmarshalling account " + args[0] + "\n err:" + err.Error())
+			return company, errors.New("Error unmarshalling account " + args[0])
 		}
 
 		//company, err := GetCompany(args[0], stub)
