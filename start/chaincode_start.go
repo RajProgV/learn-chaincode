4c3c91ae1ad6dc678c9e85031dbfdb2b702842a4
test56tester28tt <test56tester28tt@gmail.com>
2016-12-12 11:10:29 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -341,7 +341,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 				fmt.Println("Error marshalling the company")
 				return nil, err1
 			}
-			jsonResp := "{\"Name\":\"" + company + "\",\"Amount\":\"" + string(companyBytes) + "\"}"
+			jsonResp := company
 			fmt.Println("All success, returning the company :%s \n", jsonResp)
 			return companyBytes, nil
 		}
