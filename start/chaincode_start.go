2f54680e6d3f304b6b57b60801722d5601cfc3b2
test56tester28tt <test56tester28tt@gmail.com>
2016-12-15 10:57:30 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -82,10 +82,10 @@ func msToTime(ms string) (time.Time, error) {
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Printf("========================= Init called, initializing chaincode")
 
-	if len(args) == 0 {
+	/*if len(args) == 0 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 4")
-	}
-	/*var A, B string    // Entities
+	}*/
+	var A, B string    // Entities
 	var Aval, Bval int // Asset holdings
 	var err error
 
@@ -115,7 +115,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
 	if err != nil {
 		return nil, err
-	}*/
+	}
 
 	return nil, nil
 }
