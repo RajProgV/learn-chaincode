52075a79ceba2f6b8660abc699da175c9a7f1f93
test56tester28tt <test56tester28tt@gmail.com>
2016-12-15 10:36:42 +0530

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -25,8 +25,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	//"github.com/test56tester28tt/hyperledger/fabric/core/chaincode/shim"
+	//"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/test56tester28tt/hyperledger/fabric/core/chaincode/shim"
 )
 
 // SimpleChaincode example simple Chaincode implementation
